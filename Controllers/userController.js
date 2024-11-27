9fb35fc8d8529ad903a1e7d64c2fbbc9f40878d1
webpandaaa <ankushrajsingh2004@gmail.com>
2024-11-27 23:59:55 +0530

    kuch bi

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,9 +1,26 @@
-const express = require('express');
 const userModel = require("../Models/userModel");
 const expressAsyncHandler = require("express-async-handler");
+const generateToken = require("../config/generateToken");
 
 
-const loginController = () => {}
+// login
+const loginController = expressAsyncHandler(async (req, res) => {
+    const { name , password} = req.body;
+    const user = await userModel.findOne({name});
+
+    if(user && (await user.matchPassword(password))){
+        const response = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+          };
+          res.json(response);
+    }else{
+        throw new Error("Invalid Username or Password");
+    }
+})  
 
 const registerController = expressAsyncHandler (async (req, res) => {
     const {name , email , password} = req.body;
@@ -28,7 +45,18 @@ const registerController = expressAsyncHandler (async (req, res) => {
 
     //create a user in database
     const  user = await userModel.create({ name , email, password})
-    console.log("successfully created");
+    if(user){
+        res.status(201).json({
+            _id :  user._id,
+            name :  user.name,
+            email : user.email,
+            isAdmin :  user.isAdmin,
+            token : generateToken(user._id)
+        });
+    }else{
+        res.status(400);
+        throw new Error("Register Error");
+    }
 })
 
 
