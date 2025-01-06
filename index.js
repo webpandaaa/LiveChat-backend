955e4b742eaefffce64e2d9d5729673290623b5d
webpandaaa <ankushrajsingh2004@gmail.com>
2025-01-06 20:15:56 +0530

    socket adding

--- index.js
+++ index.js
@@ -42,3 +42,39 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server is Running..."));
+
+const io = require("socket.io")(server, {
+  cors : {
+    origin :  "*",
+  },
+  pingTimeout : 60000,
+});
+
+io.on("connection" , (socket) => {
+  // console.log("socket.io connection esatablished");
+  
+  socket.on("setup" , (user) =>{
+    socket.join(user.data._id);
+    // console.log("server : // joined use : " , user.data._id)
+    socket.emit("connected");
+  })
+
+  socket.on("join chat" , (room) => {
+    socket.join(room);
+    // console.log("user joined Room : " , room);
+  })
+
+
+  socket.on("new message" , (newMessageStatus) => {
+    var chat = newMessageStatus.chat;
+    if(!chat.users){
+      return console.log("chat.users not defined");
+    }
+    chat.users.forEach((user) => {
+      if(user._id == newMessageStatus.sender._id) return;
+
+      socket.in(user._id).emit("message recieved" , newMessageRecived);
+    });
+  });
+});
+
